ac12eb7f3904e325701f7563b7f6aae37c494468
mert sayar <merttsayar@gmail.com>
2023-05-09 00:40:15 +0300

    router issue fixed

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter} from "react-router-dom"
 import { LoginContextProvider } from "./contexts/loginContext"
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <LoginContextProvider>
         <QueryClientProvider client={queryClient}>
           <App />
         </QueryClientProvider>
       </LoginContextProvider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
 )
